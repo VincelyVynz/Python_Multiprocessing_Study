87d406f93d7226aa2cdca176f2be1f2738819eed
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-22 17:54:31 +0530

    Create image_processor_multiprocess.py

---
+++ image_processor_multiprocess.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import os, time
+from multiprocessing import Pool
+
+def gray_scale(image):
+    filename = os.path.basename(image)
+    os.makedirs("output_images", exist_ok=True)
+    with Image.open(image) as img:
+        print(f"Converting {filename}")
+        img = img.convert('L')
+        img.save(f'output_images/{filename}')
+        print(f"Saved {filename}")
+
+
+def main():
+    folder = input('Enter the images folder path : ')
+
+    if os.path.isdir(folder):
+        images = [os.path.join(folder, file) for file in os.listdir(folder) if file.lower().endswith(('.jpg', '.jpeg', '.png'))]
+        start_time = time.perf_counter()
+        with Pool() as pool:
+            pool.map(gray_scale, images)
+        end_time = time.perf_counter()
+        print(f"Time taken to convert {len(images)} images: {end_time - start_time} seconds")
+    else:
+        print("Folder not found.")
+
+
+if __name__ == '__main__':
+    main()
