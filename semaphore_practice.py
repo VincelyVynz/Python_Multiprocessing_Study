1f33bc9618193077facb1969a1e51d3eb2274711
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-12-24 17:11:10 +0530

    semaphore practice

---
+++ semaphore_practice.py
@@ -0,0 +1,17 @@
+import time, multiprocessing
+
+
+def worker_task(worker_id, parking_lot):
+    print(f"Worker {worker_id} is looking for a parking spot")
+
+    with parking_lot:
+        print(f"Worker {worker_id} is parked")
+        time.sleep(1.5)
+        print(f"Worker {worker_id} is leaving the parking lot")
+
+
+if __name__ == "__main__":
+
+    parking_lot = multiprocessing.Semaphore(2)
+
+    processes = [multiprocessing.Process(target=worker_task, args=(i, parking_lot)) for i in range(5)]
